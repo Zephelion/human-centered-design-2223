8b4362e42ba6c24b5ae8d403313f4a3a06337526
Zephelion <russellnumo@outlook.com>
2023-05-12 13:37:47 +0200

    :trollface:

---
+++ test2/js/cc_source.js
@@ -0,0 +1,241 @@
+export const captions = [
+    {
+        vtt: "00:00:03.000 --> 00:00:06.000",
+        start: "",
+        end: "",
+        text: [
+            {
+                speaker: null,
+                speech: "clicker grunts",
+                emotion: null,
+                text_type: "sound",
+                text_class: null
+            }
+        ],
+        cc_class: null
+    },
+    {
+        vtt: "00:00:15.000 --> 00:00:26.000",
+        start: "",
+        end: "",
+        text: [
+            {
+                speaker: null,
+                speech: "suspenseful music",
+                emotion: null,
+                text_type: "sound",
+                text_class: null
+            }
+        ],
+        cc_class: null
+    },
+
+    // {
+    //     vtt: "00:00:07.000 --> 00:00:12.000",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "Ebra!",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: "uppercase"
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:00.800 --> 00:00:02.320",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "Make sandwiches!",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: "uppercase"
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:02.320 --> 00:00:04.760",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "Don't stop. Making. Fucking sandwiches.",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: "uppercase"
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:04.780 --> 00:00:05.300",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Ebra",
+    //             speech: "Yes, Chef.",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:05.300 --> 00:00:06.870",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "Imma make three sections, OK?",
+    //             emotion: "voice still loud",
+    //             text_type: "speech",
+    //             text_class: null
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:06.870 --> 00:00:09.313",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "They're going to be Wet, Hot, and Sweet, all right?",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:09.313 --> 00:00:10.980",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "I'm gonna to take green tape, make those sections.",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         },
+    //         {
+    //             speaker: "Louie",
+    //             speech: "Yes, Chef.",
+    //             emotion: "soft",
+    //             text_type: "speech",
+    //             text_class: "small"
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:10.980 --> 00:00:12.630",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "Louie, I want you to get the sandwiches",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:12.630 --> 00:00:13.960",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "put em the corresponding sections, copy?",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:13.960 --> 00:00:14.760",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Louie",
+    //             speech: "OK, yeah yeah",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         },
+    //         {
+    //             speaker: "Someone",
+    //             speech: "Behind!",
+    //             emotion: "sounding agitated",
+    //             text_type: "speech",
+    //             text_class: null
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:15.000 --> 00:00:17.320",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "Yo, Sweeps, bag, Sharpie, label that shit.",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         },
+    //         {
+    //             speaker: "Sydeny",
+    //             speech: "Behind..!",
+    //             emotion: "voice distant",
+    //             text_type: "speech",
+    //             text_class: "small"
+    //         }
+    //     ],
+    //     cc_class: null
+    // },
+    // {
+    //     vtt: "00:00:17.320 --> 00:00:18.780",
+    //     start: "",
+    //     end: "",
+    //     text: [
+    //         {
+    //             speaker: "Camren",
+    //             speech: "Please, Chef",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         },
+    //         {
+    //             speaker: "Sweeps",
+    //             speech: "Yes, Chef.",
+    //             emotion: null,
+    //             text_type: "speech",
+    //             text_class: null
+    //         },
+    //     ],
+    //     cc_class: null
+    // },
+]
